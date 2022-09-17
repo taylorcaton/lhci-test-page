1a353b67b711eb5bbc39d1763dedcbcd1e16a11f
Taylor Caton <taylorcaton@gmail.com>
2022-09-16 22:53:44 -0400

    Add a placeholder image

--- src/App.vue
+++ src/App.vue
@@ -5,9 +5,13 @@
   <header>
     <div class="wrapper">
       <h1>Hello World!</h1>
+      <img src="https://picsum.photos/600/" alt="Placeholder Image">
     </div>
   </header>
 </template>
 
 <style scoped>
+  h1 {
+    text-align: center;
+  }
 </style>
